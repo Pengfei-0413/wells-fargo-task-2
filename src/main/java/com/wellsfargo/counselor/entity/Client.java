a75b5097b7dff90cf7f42341b80b36688c7b1aa9
Pengfei <lpffffei@gmail.com>
2025-02-16 00:52:11 +1100

    111

--- src/main/java/com/wellsfargo/counselor/entity/Client.java
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -7,10 +7,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity
-public class Advisor {
-
+public class Client {
     @Id
     @GeneratedValue()
+    private long clientId;
+
+    @Column(nullable = false)
     private long advisorId;
 
     @Column(nullable = false)
@@ -28,11 +30,11 @@ public class Advisor {
     @Column(nullable = false)
     private String email;
 
-    protected Advisor() {
-
+    public Client() {
     }
-    //test
-    public Advisor(String firstName, String lastName, String address, String phone, String email) {
+
+    public Client(long advisorId, String firstName, String lastName, String address, String phone, String email) {
+        this.advisorId = advisorId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -40,8 +42,16 @@ public class Advisor {
         this.email = email;
     }
 
-    public Long getAdvisorId() {
+    public long getClientId() {
+        return clientId;
+    }
+
+    public long getAdvisorId() {
         return advisorId;
+    }
+
+    public void setAdvisorId(long advisorId) {
+        this.advisorId = advisorId;
     }
 
     public String getFirstName() {
---
+++ src/main/java/com/wellsfargo/counselor/entity/Security.java
@@ -0,0 +1,98 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Security {
+    @Id
+    @GeneratedValue()
+    private long securityId;
+
+    @Column(nullable = false)
+    private String portfolioId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private String purchasePrice;
+
+    @Column(nullable = false)
+    private Date purchaseDate;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    public Security() {
+    }
+
+    public Security(String portfolioId, String name, String category, String purchasePrice, Date purchaseDate, Integer quantity) {
+        this.portfolioId = portfolioId;
+        this.name = name;
+        this.category = category;
+        this.purchasePrice = purchasePrice;
+        this.purchaseDate = purchaseDate;
+        this.quantity = quantity;
+    }
+
+    public long getSecurityId() {
+        return securityId;
+    }
+
+
+    public String getPortfolioId() {
+        return portfolioId;
+    }
+
+    public void setPortfolioId(String portfolioId) {
+        this.portfolioId = portfolioId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(String purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
